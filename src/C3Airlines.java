72e8cdae790c66c1ca5921cac357a0362b0a2136
AndreiBoeck <andreicunhaboeck@gmail.com>
2023-06-16 11:46:18 -0300

    implementar: quem destino aeronave preço assento tipo de assento

--- src/C3Airlines.java
+++ src/C3Airlines.java
@@ -4,18 +4,21 @@ import java.io.PrintStream;
 
 public class C3Airlines {
     public static void main(String[] args) throws FileNotFoundException {
-        PrintStream file = new PrintStream("Boeing 767-300ER");
-        Boolean[][] boeing = new Boolean[5][45];
+        PrintStream file = new PrintStream("src/Boeing 767-300ER");
+        Boolean[][] boeing = new Boolean[45][7];
         Boolean preencher = false;
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 45; j++) {
+        for (int i = 0; i < 45; i++) {
+            for (int j = 0; j < 7; j++) {
                 boeing[i][j] = preencher;
             }
         }
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 45; j++) {
+        for (int i = 0; i < 45; i++) {
+            for (int j = 0; j < 7; j++) {
                 file.print(boeing[i][j]);
+                file.print(" ");
             }
+            file.println();
+
         }
     }
 }
--- src/library/user.java
+++ src/library/user.java
@@ -1,5 +1,9 @@
 package library;
 
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+
 public class user {
 
+    }
 }
