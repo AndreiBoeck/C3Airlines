926ee4f36a6601bf1d107b1aa3db14e77899a7a8
AndreiBoeck <andreicunhaboeck@gmail.com>
2023-06-16 22:54:09 -0300

    possivel ler e gravar em arquivo txt

--- src/C3Airlines.java
+++ src/C3Airlines.java
@@ -1,24 +1,18 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.PrintStream;
+import java.util.Scanner;
+
+import library.user;
 
 public class C3Airlines {
     public static void main(String[] args) throws FileNotFoundException {
-        PrintStream file = new PrintStream("src/Boeing 767-300ER");
-        Boolean[][] boeing = new Boolean[45][7];
-        Boolean preencher = false;
-        for (int i = 0; i < 45; i++) {
-            for (int j = 0; j < 7; j++) {
-                boeing[i][j] = preencher;
-            }
-        }
-        for (int i = 0; i < 45; i++) {
-            for (int j = 0; j < 7; j++) {
-                file.print(boeing[i][j]);
-                file.print(" ");
-            }
-            file.println();
-
-        }
+        user teste = new user();
+        Scanner in = new Scanner(System.in);
+        String pride = "";
+        do {
+            pride = in.next();
+            teste.hold(pride);
+        } while (!pride.startsWith("sair"));
+        teste.write(teste.h);
     }
 }
